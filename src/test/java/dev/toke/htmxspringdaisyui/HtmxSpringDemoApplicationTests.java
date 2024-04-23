d549bbea5d52d25644527fe1e45c11218493f9a6
Jack Toke <jackptoke@gmail.com>
2024-04-24 06:38:33 +1000

    first commit

---
+++ src/test/java/dev/toke/htmxspringdaisyui/HtmxSpringDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.toke.htmxspringdaisyui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HtmxSpringDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
